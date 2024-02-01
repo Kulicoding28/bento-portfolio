1431ca59ab00ed84c7e4e779c97a0b3ce281d3ca
Kulicoding28 <ahmadabdulaziz380@gmail.com>
2024-02-01 13:33:40 +0700

    edit-link

--- app/components/grid/projects.tsx
+++ app/components/grid/projects.tsx
@@ -6,16 +6,18 @@ const Project = ({ item }: { item: GridItemInterface }) => {
   return (
     <Link href={item.buttonLink ?? ""} target="_blank">
       <div className="flex flex-col items-center">
-        <p className="@lg:text-lg font-semibold  line-clamp-1 mt-0 xl:mt-8">
-          {item.title}
-        </p>
-        <p className="text-sm text-neutral-500 ">{item.tech}</p>
+        <Image
+          className="z-0 object-cover object-center w-full h-full"
+          alt={item.title}
+          src={item.image as string}
+        />
+        <div className="mb-20 p-2 items-center text-center">
+          <p className="@lg:text-lg font-semibold  line-clamp-1">
+            {item.title}
+          </p>
+          <p className="text-sm text-neutral-500 ">{item.tech}</p>
+        </div>
       </div>
-      <Image
-        className="z-0 object-cover object-center w-full h-full"
-        alt={item.title}
-        src={item.image as string}
-      />
     </Link>
   );
 };
