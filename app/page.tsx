5bf7a0e5270eaed9d3cca2cbb203ea4063085ce6
Kulicoding28 <ahmadabdulaziz380@gmail.com>
2024-02-06 10:17:30 +0700

    edit-nextConfig

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,20 @@
-import { Fragment } from "react";
 import LeftProfile from "./components/left-profile";
 import RightPanel from "./components/right-panel";
 
 export default function Home() {
   return (
-    <Fragment>
-      <main className="relative flex flex-col items-center flex-1 w-full h-full ">
-        {/* bg-pattern */}
-        <div className="absolute inset-0 z-0 bg-light-pattern dark:bg-dark-pattern bg-verySmall" />
-        {/* overlay */}
-        <div className="absolute inset-0 z-10 bg-gradient-to-b from-white via-white/90 to-white dark:from-neutral-950 dark:via-neutral-950/90 dark:to-neutral-950" />
-        <div className="container relative z-10 flex flex-col w-full h-full  px-6  gap-4 xl:gap-10 xl:flex-row">
-          {/* left-profile */}
-          <LeftProfile />
-          {/* Right Panel */}
+    <main className="relative flex flex-col items-center flex-1 w-full h-full ">
+      {/* bg-pattern */}
+      <div className="absolute inset-0 z-0 bg-light-pattern dark:bg-dark-pattern bg-verySmall" />
+      {/* overlay */}
+      <div className="absolute inset-0 z-10 bg-gradient-to-b from-white via-white/90 to-white dark:from-neutral-950 dark:via-neutral-950/90 dark:to-neutral-950" />
+      <div className="container relative z-10 flex flex-col w-full h-full  px-6  gap-4 xl:gap-10 xl:flex-row">
+        {/* left-profile */}
+        <LeftProfile />
+        {/* Right Panel */}
 
-          <RightPanel />
-        </div>
-      </main>
-    </Fragment>
+        <RightPanel />
+      </div>
+    </main>
   );
 }
